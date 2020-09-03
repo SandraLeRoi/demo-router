969afa6156536d69618a88a408812d3b89b15823
Sandra <oawx72@gmail.com>
2020-09-03 10:31:22 +0200

    apprentissage de react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Homepage from "./Page/Homepage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Link to="/bonjour/1">liens 1 </Link>
+          <Link to="/bonjour/2">liens 2 </Link>
+          <Link to="/bonjour/3">liens 3 </Link>
+       <Switch>
+           <Route exact path="/">
+               <Homepage/>
+           </Route>
+           <Route path="/bonjour/1">
+               <h1>Salut</h1>
+           </Route>
+           <Route path="/bonjour/2">
+               <h1>à</h1>
+           </Route>
+           <Route path="/bonjour/3">
+               <h1>Tous</h1>
+           </Route>
+           <Route path="/">
+               <h1>404</h1>
+           </Route>
+       </Switch>
+      </Router>
     </div>
   );
 }
